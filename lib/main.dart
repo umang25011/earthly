944a6aaf9ed0007113d06d23638c38a5fafb325c
Umang Patel <umang25011@gmail.com>
2022-05-22 09:51:07 -0400

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {}
+}
