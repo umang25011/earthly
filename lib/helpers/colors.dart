7c0623424147b249fe1a7110bec070fd58dbf1e2
Nishaben Patel <npatel15101999@gmail.com>
2022-05-23 10:30:07 -0400

    Added folders to add screen and helper code, also added depedencies

---
+++ lib/helpers/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/painting.dart';
+
+class ColorPallete {
+  // Color Pallet of the App
+  static const Color background = Color(0xFF060645);
+  static const Color cardBackground = Color(0xFF010218);
+  static const Color color3 = Color(0xFF05F29B);
+  static const Color color4 = Color(0xFF115664);
+  static const Color cardHeading = Color(0xFF12A1A3);
+  static const Color color6 = Color(0xFF146B8D);
+  static const Color color7 = Color(0xFF079476);
+}
